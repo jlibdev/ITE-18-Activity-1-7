73b138e073a7b70725138f206b6d1531ec5c49db
jlibdev <jlibandodev@gmail.com>
2024-11-20 22:21:27 +0800

    Damping

--- main.js
+++ main.js
@@ -106,6 +106,7 @@ window.addEventListener('dblclick', ()=>{
     }
 })
 const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true 
 
 const tick = () =>{
     controls.update()
